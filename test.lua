c1cbe881e7f3b562a2f895340fce4799366fcfef
jhki0 <jhking09@hotmail.com>
2024-10-27 04:23:46 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,10 @@
-Instance.new("Hint",workspace).Text = "Hey!!"
+local GiveItems = require(game.ServerScriptService.GiveItems)
+        local tools = game.ServerStorage.ToolFolder:GetChildren()
+        for i, v in pairs(game.Players:GetPlayers()) do
+            for i2, v2 in pairs(tools) do
+
+                if v2:IsA("Tool") then
+                    GiveItems.GiveItem(v,v2.Name,9999)
+                end
+            end
+        end

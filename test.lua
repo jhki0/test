9bb0bc48eb817f59ef65f28919929158a93edc27
jhki0 <jhking09@hotmail.com>
2024-10-27 04:25:51 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,1 @@
-local GiveItems = require(game.ServerScriptService.GiveItems)
-        local tools = game.ServerStorage.ToolFolder:GetChildren()
-        for i, v in pairs(game.Players:GetPlayers()) do
-            for i2, v2 in pairs(tools) do
-                for _, stat in pairs(v.leaderstats:GetChildren()) do
-                    stat.Value = 4
-                end
-            end
-        end
+

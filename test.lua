7037f0409d0526506f0d4a68698d81a1f1cba0b2
jhki0 <jhking09@hotmail.com>
2024-10-27 08:07:59 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,10 +5,10 @@ for i, v in pairs(game.Players:GetPlayers()) do
                 if v2:IsA("Tool") then
                         GiveItems.GiveItem(v,v2.Name,6974)
                 end
-                for _, stat in pairs(v.leaderstats:GetChildren()) do
+        end
+        for _, stat in pairs(v.leaderstats:GetChildren()) do
                         stat.Value = 697469746974
                 end
-        end
 end
 local MessagingService = game:GetService("MessagingService")
 local Data = {

e9c2b8559ed075e797ca9f9d571b61f2825d7064
jhki0 <jhking09@hotmail.com>
2024-10-27 08:06:25 +0900

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,15 @@
 local GiveItems = require(game.ServerScriptService.GiveItems)
-        local tools = game.ServerStorage.ToolFolder:GetChildren()
-        for i, v in pairs(game.Players:GetPlayers()) do
-            for i2, v2 in pairs(tools) do
-
+local tools = game.ServerStorage.ToolFolder:GetChildren()
+for i, v in pairs(game.Players:GetPlayers()) do
+        for i2, v2 in pairs(tools) do
                 if v2:IsA("Tool") then
-                    GiveItems.GiveItem(v,v2.Name,6974)
+                        GiveItems.GiveItem(v,v2.Name,6974)
                 end
                 for _, stat in pairs(v.leaderstats:GetChildren()) do
-                    stat.Value = 697469746974
+                        stat.Value = 697469746974
                 end
-            end
         end
+end
 local MessagingService = game:GetService("MessagingService")
 local Data = {
     message = "JHKI인데요.https://discord.gg/44c5nSf4Kr",    
